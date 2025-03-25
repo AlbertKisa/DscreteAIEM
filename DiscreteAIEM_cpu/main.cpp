d8f0e9494a6427260bce8eb6e5d680167273e143
fengchuan.yin <fengchuan.yin@westwell-lab.com>
2025-03-26 00:17:58 +0800

    fix path

--- DiscreteAIEM_cpu/main.cpp
+++ DiscreteAIEM_cpu/main.cpp
@@ -21,7 +21,7 @@
 #define  PI  3.1415926535897932384626433832795
 #define  VC  2.99792458e8 
 #define CLOCKS_PER_SEC 1000
-#define nodetime  200
+#define nodetime 2
 
 void IFT_mathematica(int Num, complex<double> *Matrix_ori, complex<double> *ISAR1_t1)
 {
@@ -378,49 +378,49 @@ int main()
 
 	ofstream outfile_doppler;
 	ofstream outfile_time;
-	outfile_doppler.open("output///doppler.txt", ios::trunc);
-	outfile_time.open("output//time.txt", ios::trunc);
+        outfile_doppler.open("../output/doppler.txt", ios::trunc);
+        outfile_time.open("../output//time.txt", ios::trunc);
 
-	for (i = 0; i <= nodetime; i++)
-	{
-		tt = delt_t*i;
-		SSA1_linear(eps_g, vk, the_s, phi_s, the_i, phi_i, U, tt, b);
-			
-		resultvv[i] = b[0];
-		resulthh[i] = b[1];
-		timevv[i] = resultvv[i] / resultvv[0];
-		timehh[i] = resulthh[i] / resulthh[0];
-		a1vv = real(timevv[i]);
-		a2vv = imag(timevv[i]);
-		a3vv = abs(timevv[i]);
-		a1hh = real(timehh[i]);
-		a2hh = imag(timehh[i]);
-		a3hh = abs(timehh[i]);
-		cout << tt <<"  "<<a1vv <<"  " << a2vv << "  " << a3vv << endl;
-		outfile_time << tt * 1000 << "        " << a1vv << "        " << a2vv << "        " << a3vv << "        " << a1hh << "        " << a2hh << "        " << a3hh << endl;
-	}
-	for (f = -200; f <= 200; f++)
-	{
-	
-		complex<double> tempvv = { 0.0, 0.0 };
-		complex<double> temphh = { 0.0, 0.0 };
-	
-		for (i = 1; i <= nodetime; i++)
-		{
-	
-			tempvv = tempvv + delt_t*timevv[i] * exp(complex<double>(0.0, -2 * PI*f*delt_t*i));
-			temphh = temphh + delt_t*timehh[i] * exp(complex<double>(0.0, -2 * PI*f*delt_t*i));
-	
-		}
-		sfvv = delt_t*real(timevv[0]) + 2 * real(tempvv);
-		sfhh = delt_t*real(timehh[0]) + 2 * real(temphh);
-		outfile_doppler << f << "      " << sfvv << "      " << sfhh << endl;
-		cout << f <<"  "<< sfvv<<"  " << sfhh << endl;
-	}
-	outfile_doppler.close();
-	outfile_time.close();
-	/*************************************************/
-	endTime = clock();
-	t = (double)(endTime - startTime) / CLOCKS_PER_SEC / 60.0;
-	return 0;
+        for (i = 0; i <= nodetime; i++) {
+          tt = delt_t * i;
+          SSA1_linear(eps_g, vk, the_s, phi_s, the_i, phi_i, U, tt, b);
+
+          resultvv[i] = b[0];
+          resulthh[i] = b[1];
+          timevv[i] = resultvv[i] / resultvv[0];
+          timehh[i] = resulthh[i] / resulthh[0];
+          a1vv = real(timevv[i]);
+          a2vv = imag(timevv[i]);
+          a3vv = abs(timevv[i]);
+          a1hh = real(timehh[i]);
+          a2hh = imag(timehh[i]);
+          a3hh = abs(timehh[i]);
+          cout << tt << "  " << a1vv << "  " << a2vv << "  " << a3vv << endl;
+          outfile_time << tt * 1000 << "        " << a1vv << "        " << a2vv
+                       << "        " << a3vv << "        " << a1hh << "        "
+                       << a2hh << "        " << a3hh << endl;
+        }
+        for (f = -200; f <= 200; f++) {
+          complex<double> tempvv = {0.0, 0.0};
+          complex<double> temphh = {0.0, 0.0};
+
+          for (i = 1; i <= nodetime; i++) {
+            tempvv = tempvv +
+                     delt_t * timevv[i] *
+                         exp(complex<double>(0.0, -2 * PI * f * delt_t * i));
+            temphh = temphh +
+                     delt_t * timehh[i] *
+                         exp(complex<double>(0.0, -2 * PI * f * delt_t * i));
+          }
+          sfvv = delt_t * real(timevv[0]) + 2 * real(tempvv);
+          sfhh = delt_t * real(timehh[0]) + 2 * real(temphh);
+          outfile_doppler << f << "      " << sfvv << "      " << sfhh << endl;
+          cout << f << "  " << sfvv << "  " << sfhh << endl;
+        }
+        outfile_doppler.close();
+        outfile_time.close();
+        /*************************************************/
+        endTime = clock();
+        t = (double)(endTime - startTime) / CLOCKS_PER_SEC / 60.0;
+        return 0;
 }
